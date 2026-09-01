d3225063000ac495c5b98c7d9a1138733d52292c
ajaymaurya555 <ajaymaurya555@gmail.com>
2026-09-01 12:26:55 +0530

    update Moudle_05

---
+++ Moudle_06/String.py
@@ -0,0 +1,42 @@
+# String
+print("Hello Guido Van Russom Sir.!")
+print('Hello Guido Van Russom Sir.!')
+print('''Hello Guido Van Russom Sir.!''')
+
+# String Slicing
+
+# EXAMPE !
+
+text = "python"
+print(text[1:4])
+
+text = "python"
+print(text[:3])
+
+text = "python"
+print(text[3:])
+
+text = "python"
+print(text[-4:-1])
+
+
+text = "python"
+print(text[: : 2])
+
+text = "python"
+print(text[: : -1])
+
+
+text = "Programming"
+print(text[: : -2])
+
+
+text = "Programming"
+print(text[2:8:2])
+
+
+text = "Computer"
+print(text[5:2:-1])
+
+text = "Computer"
+print(text[-1:-6:-1])
