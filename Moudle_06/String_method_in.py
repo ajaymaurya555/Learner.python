4a874e9966a55a40e209b76cddd5cf3e6cd41250
ajaymaurya555 <ajaymaurya555@gmail.com>
2026-09-10 00:50:26 +0530

    Update Module_06

---
+++ Moudle_06/String_method_in.py
@@ -0,0 +1,76 @@
+name ="ajay kr. Maurya"
+print(name.upper())
+
+name ="AJAY KR. MAURYA"
+print(name.lower())
+
+name ="ajay kr. maurya"
+print(name.capitalize())
+
+name ="ajay kr. maurya"
+print(name.title())
+
+name ="  python  "
+print(name.strip())
+
+text="I love python"
+print(text.replace("python","java"))
+
+text = "python programming"
+print(text.find("programming"))
+
+text = "Ajay kumar maurya"
+print(text.find("kumar"))
+
+text = "banana"
+print(text.count("a"))
+
+text ="Python programming"
+print(text.startswith("Python"))
+
+text ="Python programming"
+print(text.startswith("python"))
+
+text ="Python programming"
+print(text.endswith("Python"))
+
+text ="Python programming"
+print(text.endswith("programming"))
+
+text ="Python programming"
+print(text.endswith("programming"))
+
+text ="Python programming"
+print(text.endswith("Programming"))
+
+word = ["python", "is","Eassy"]
+print(" ".join(word))
+
+text = "Ajay Kr. Maurya"
+print(text.isalpha())
+
+text = "ajay1234"
+print(text.isalnum())
+
+text = "A1234"
+print(text.isnumeric())
+
+text = "1234"
+print(text.isnumeric())
+
+text = "ajay1234.67"
+print(text.isdecimal())
+
+text = "1232"
+print(text.isdecimal())
+
+text = "1234"
+print(text.isdigit())
+
+text = "Ajay"
+print(text.isidentifier())
+text = "Ajay1"
+print(text.isidentifier())
+text = "_Ajay"
+print(text.isidentifier())
+
---
+++ Moudle_06/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+text = "Ajay"
+print(text.isidentifier())
+text = "Ajay1"
+print(text.isidentifier())
+text = "_Ajay"
+print(text.isidentifier())
